9d1239bd6d8f836d2568022fc43d8ca8f1fa490b
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2015-04-07 15:07:59 +1000

    Actually, point to the github page version

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ import os
 
 AUTHOR = u'AMIGOS Team'
 SITENAME = u'AMIGOS - Australian MIcrobial GenOmics Symposium'
-SITEURL = 'http://theamigos.space'
+SITEURL = 'http://the-amigos.github.io'
 SITELOGO = 'images/AMIGOS_logo.png'
 
 PATH = 'content'
